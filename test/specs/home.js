a29a74113765c83a6566a8a129dacbeedc393126
Joohoon Kim <jayjhkim7@gmail.com>
2021-10-25 19:28:19 -0400

    test text

--- test/specs/home.js
+++ test/specs/home.js
@@ -2,10 +2,21 @@
 // https://practice.automationbro.com/
 
 describe('Home', () => {
-  it('logo click leads to url that doesn not contain get-started text', async () => {
+  // it('logo click leads to url that doesn not contain get-started text', async () => {
+  //   await browser.url('https://practice.automationbro.com');
+  //   // await $('//img[@alt="Practice E-Commerce Site"]').click();
+  //   const logo = await $('.custom-logo');
+  //   await logo.click();
+  //   await expect(browser).not.toHaveUrlContaining('get-started');
+  // });
+
+  it('find heading element and assert its text', async () => {
     await browser.url('https://practice.automationbro.com');
-    // await $('//img[@alt="Practice E-Commerce Site"]').click();
-    await $('.custom-logo').click();
-    await expect(browser).not.toHaveUrlContaining('get-started');
+
+    const elem = await $('.elementor-widget-container > h1');
+    console.log('ELEM: ', elem);
+    // const heading = await elem.getText();
+    // await expect(heading).toEqual('Think different. Make different.');
+    await expect(elem).toHaveText('Think different. Make different.');
   });
 });
