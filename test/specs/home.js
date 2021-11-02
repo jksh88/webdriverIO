e0af889282bdf60f2e27e343b394f60e72f61025
Joohoon Kim <jayjhkim7@gmail.com>
2021-11-01 21:01:25 -0400

    before hook

--- test/specs/home.js
+++ test/specs/home.js
@@ -1,11 +1,11 @@
 import HomePage from '../pages/home-page.js';
 
 describe('Home', () => {
-  it('Open url and assert title', async () => {
+  before('Open home url', async () => {
     await HomePage.open();
   });
 
-  it('Open About Page and assert url', async () => {
+  xit('Open About Page and assert url', async () => {
     await browser.url('/about');
     await expect(browser).toHaveUrl(
       'https://practice.automationbro.com/about/'
@@ -13,19 +13,19 @@ describe('Home', () => {
   });
 
   it('Click get started btn and assert url contains get-started text', async () => {
-    await HomePage.open();
+    // await HomePage.open();
     await HomePage.startedBtn.click();
     await expect(browser).toHaveUrlContaining('get-started');
   });
 
   it('logo click leads to url that doesn not contain get-started text', async () => {
-    await HomePage.open();
+    // await HomePage.open();
     await HomePage.customLogo.click();
     await expect(browser).not.toHaveUrlContaining('get-started');
   });
 
   it('find heading element and assert its text', async () => {
-    await HomePage.open();
+    // await HomePage.open();
     await expect(HomePage.slogan).toHaveText(
       'Think different. Make different.'
     );
