ea99cd7366feaac7c80bd913ef809604ef7f5e88
Joohoon Kim <jayjhkim7@gmail.com>
2021-11-07 11:17:24 -0500

    changed back to browserstack in config

--- wdio.conf.js
+++ wdio.conf.js
@@ -125,8 +125,8 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: ['selenium-standalone'],
-  //JK_choices: chromedriver, browserstack
+  services: ['browserstack'],
+  //JK_choices: chromedriver, selenium-standalone, browserstack
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
