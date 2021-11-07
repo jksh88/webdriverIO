c499aca11ef3a6acb8ec6f4ebbfdd99fad2464ab
Joohoon Kim <jayjhkim7@gmail.com>
2021-11-07 08:22:21 -0500

    commented out oncomplete hook body

--- wdio.conf.js
+++ wdio.conf.js
@@ -295,22 +295,19 @@ exports.config = {
    * @param {<Object>} results object containing test results
    */
   onComplete: function () {
-    const reportError = new Error('Could not generate Allure report');
-    const generation = allure(['generate', 'allure-results', '--clean']);
-    return new Promise((resolve, reject) => {
-      const generationTimeout = setTimeout(() => reject(reportError), 5000);
-
-      generation.on('exit', function (exitCode) {
-        clearTimeout(generationTimeout);
-
-        if (exitCode !== 0) {
-          return reject(reportError);
-        }
-
-        console.log('Allure report successfully generated');
-        resolve();
-      });
-    });
+    // const reportError = new Error('Could not generate Allure report');
+    // const generation = allure(['generate', 'allure-results', '--clean']);
+    // return new Promise((resolve, reject) => {
+    //   const generationTimeout = setTimeout(() => reject(reportError), 5000);
+    //   generation.on('exit', function (exitCode) {
+    //     clearTimeout(generationTimeout);
+    //     if (exitCode !== 0) {
+    //       return reject(reportError);
+    //     }
+    //     console.log('Allure report successfully generated');
+    //     resolve();
+    //   });
+    // });
   },
   /**
    * Gets executed when a refresh happens.
